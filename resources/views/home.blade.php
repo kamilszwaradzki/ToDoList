7e79551829a7fcb13f849c5c341a095f46ef187e
kamil <kamil.szwaradzki@gmail.com>
2020-07-03 03:47:43 +0200

    adding control button to change status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,56 +12,61 @@
 		<script type="text/javascript" charset="utf8" src="{{asset('js/popper.min.js')}}" ></script>
 		<script type="text/javascript" charset="utf8" src="{{asset('js/bootstrap.min.js')}}" ></script>
 
-		<script>
+        <script>
 		
-			$('a[data-toggle="tab"]').on( 'shown.bs.tab', function (e) {
-				$.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
-			} );
-		 
-			$(document).ready(function() {
-				$('#myTable').DataTable();
-			} );
-			
-            $( document ).on( "click", function( event ) {
-               /*
-                    Function change status when click on "done" button from done to undone and vice versa.
-               */
-               
-               var bid = event.target.id; // button ID 
-               var trid = $(event.target).closest('tr').attr('id'); // table row ID 
-               console.log(bid,trid);
-               
-             });
-            
-		</script>
+        $('a[data-toggle="tab"]').on( 'shown.bs.tab', function (e) {
+            $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
+        } );
+     
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        } );
+        
+</script>
+
 	</head>
     <body>
         <table id="myTable" class="table table-striped table-bordered" style="width:80%; font-size:18px;">
             <thead style="position:sticky; top:0px; border-collapse:collapsed; background:#fff;">
+                <th>Id</th>
                 <th>Content</th>
                 <th>Status</th>
-                <th></th>
+                <th>Control buttons</th>
             </thead>
             <tbody>
             @for ($i = 0; $i < 100; $i++)
                 <tr id="row_{{$i}}">
-                    <td>Content of task{{$i}}</td>
-                    <td><h5><img src="{{asset('icons/checked.png')}}"/> done</h5> <!-- <input type="checkbox" name="status" value="" style="font-color:red;" checked="true"> --></td>
+                    <td>{{$i}}</td>
+                    <td>Content of task</td>
+                    <td><h5 id="status_{{$i}}"><img src="{{asset('icons/checked.png')}}"/> done</h5> <!-- <input type="checkbox" name="status" value="" style="font-color:red;" checked="true"> --></td>
                     <td>
                         <button id="done" class="btn btn-outline-primary" style="margin:10px;">Done</button>
-                        <button id="delete" class="btn btn-outline-primary" style="margin:10px;">Delete</button>
-                    </td>
-                </tr>
-                <tr id="row_{{$i}}">
-                    <td>Content of task{{$i}}</td>
-                    <td><h5> <img src="{{asset('icons/unchecked.png')}}"> undone</h5><!--<input type="checkbox" name="status" value="" style="font-color:red;" checked="false">--></td>
-                    <td>
-                        <button id="done" class="btn btn-outline-primary" style="margin:10px;">Done</button>
-                        <button id="delete" class="btn btn-outline-primary" style="margin:10px;">Delete</button>
                     </td>
                 </tr>
             @endfor
             </tbody>
         </table>
+        <script>
+        $("button").click( function(){
+        /*
+            Function change status when click on "done" button from done to undone and vice versa.
+        */
+            var bid = event.target.id; // button ID 
+            var trid = $(event.target).closest('tr').attr('id'); // table row ID 
+            console.log(bid,trid);
+            if($("#"+trid.replace("row_","status_")).text().includes("done"))
+            {
+                $("#"+trid.replace("row_","status_")).replaceWith("<h5 id='"+trid.replace("row_","status_")+"'><img src='/icons/unchecked2.png'/> unfinished</h5>");
+            }
+            else{
+                $("#"+trid.replace("row_","status_")).replaceWith("<h5 id='"+trid.replace("row_","status_")+"'><img src='/icons/checked.png'/> done</h5>");
+            }
+            console.log($("#"+trid.replace("row_","status_")).text());
+        
+        });
+
+    </script>
+
     </body>
+    
 </html>
